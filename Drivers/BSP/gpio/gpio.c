49e56f18faf29e3f101336ebd47e978926393e9e
jasond-embedded <264399714+jasond-embedded@users.noreply.github.com>
2026-03-17 17:01:02 +0100

    feat(gpio) : implement GPIO_ResetPin (register level (BSRR) reset pin)

--- Drivers/BSP/gpio/gpio.c
+++ Drivers/BSP/gpio/gpio.c
@@ -67,3 +67,16 @@ GPIO_Status_t GPIO_SetPin(GPIO_TypeDef *port, uint8_t pin) {
 
     return BSP_GPIO_OK;
 }
+
+GPIO_Status_t GPIO_ResetPin(GPIO_TypeDef *port, uint8_t pin) {
+    if (port == NULL)
+        return BSP_GPIO_ERROR;
+    if (pin > 15)
+        return BSP_GPIO_INVALID;
+
+    port->BSRR = (0x1 << pin + 16);
+
+    return BSP_GPIO_OK;
+}
+
+
